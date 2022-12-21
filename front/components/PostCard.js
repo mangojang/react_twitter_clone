7d66577047b3c98473c19dde35d1e01a9c46b801
mangojang994 <mangojang994@gmail.com>
2022-12-22 00:08:59 +0900

    Add: 댓글 컴포넌트 추가

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,30 +1,82 @@
-import React from 'react';
-import { Card, Avatar } from 'antd';
+import React, { useCallback, useState } from 'react';
+import { Card, Avatar, Form, Input, Button, List } from 'antd';
+import { Comment } from '@ant-design/compatible';
 import { RetweetOutlined, HeartOutlined, EllipsisOutlined, MessageOutlined } from '@ant-design/icons';
 import Proptypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { ADD_COMMENT_REQUEST } from '../reducers/post';
 
 const { Meta } = Card;
+const { TextArea } = Input;
 
 const postCard = ({post}) => {
-    return (
-        <Card 
-            key={+post.createdAt}
-            cover={post.img && <img alt='example' src={post.img}/>}
-            actions={[
-                <RetweetOutlined key="retweet" />,
-                <HeartOutlined key="heart" />,
-                <MessageOutlined key="message" />,
-                <EllipsisOutlined key="ellipsis" />,
-            ]}
-            style={{marginBottom: '10px'}}
+    
+    const [commentFormOpend, setCommentFormOpend] = useState(false);
+    const [commentContent, setCommentContent] = useState('');
+
+    const {mine}= useSelector(state=>state.user);
+    const dispatch = useDispatch();
+
+    const onToggleComment = useCallback(()=>{
+        setCommentFormOpend(prev => !prev);
+    },[]); 
+
+    const onSubmitComment = useCallback(()=>{
+        if(!mine){
+            return alert('로그인이 필요 합니다.')
             
-        >
-            <Meta
-            avatar={<Avatar>{post.User.nickname}</Avatar>}
-            title={post.User.nickname}
-            description={post.content}
-            />
-        </Card>
+        }
+        return dispatch({
+            type:ADD_COMMENT_REQUEST,
+        })
+    },[mine]);
+
+    const onChangeContent = useCallback((e)=>{
+        setCommentContent(e.target.value);
+    },[])
+
+    return (
+        <div style={{marginBottom: '10px'}}> 
+            <Card 
+                key={+post.createdAt}
+                cover={post.img && <img alt='example' src={post.img}/>}
+                actions={[
+                    <RetweetOutlined key="retweet" />,
+                    <HeartOutlined key="heart" />,
+                    <MessageOutlined key="message" onClick={onToggleComment}/>,
+                    <EllipsisOutlined key="ellipsis" />,
+                ]}
+            >
+                <Meta
+                avatar={<Avatar>{post.User.nickname}</Avatar>}
+                title={post.User.nickname}
+                description={post.content}
+                />
+            </Card>
+            {commentFormOpend && (
+                <>
+                    <Form onFinish={onSubmitComment}>
+                        <TextArea name="comment_content" placeholder="댓글을 입력해주세요" value={commentContent} onChange={onChangeContent} maxLength={140} style={{marginTop: '20px'}}/>
+                        <Button type='primary' htmlType='submit' loading={false} style={{marginTop: '10px'}}>전송</Button>
+                    </Form>
+                    <List
+                        header={`${post.Comments ? post.Comments.length : 0} 댓글`}
+                        itemLayout="horizontal"
+                        dataSource={post.Comments || []}
+                        renderItem={item=>(
+                            <li>
+                                <Comment
+                                    author={item.User.nickname}
+                                    avatar={<Avatar>{item.User.nickname}</Avatar>}
+                                    content={item.content}
+                                    datetime={item.createdAt}
+                                />
+                            </li>
+                        )}
+                    />
+                </>
+            )}
+        </div>
     );
 };
 
