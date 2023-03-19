102fdbc7d93b81c18fd2c8ef22f2751c1e0d74d6
mangojang994 <mangojang994@gmail.com>
2023-03-19 19:09:57 +0900

    Add : bundler-analyzer 추가

--- front/next.config.js
+++ front/next.config.js
@@ -1,8 +1,22 @@
-const nextConfig = {
-    reactStrictMode: true,
-    compiler: {
-      styledComponents: true,
-    },
-  }
+const withBundleAnalyzer = require('@next/bundle-analyzer');
+
+const nextConfig = withBundleAnalyzer( {
+  enabled: process.env.ANALYZE === 'true',
+  distDir : '.next',
+  webpack(config){
+    // console.log('config',config);
+    const prod = process.env.NODE_ENV === 'production';
+    return {
+      ...config,
+      mode: prod? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+
+    }
+  },
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+});
   
-  module.exports = nextConfig
+module.exports = nextConfig
