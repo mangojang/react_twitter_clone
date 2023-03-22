ac54c7ce2b8b38ced9249f273782ad15414f60b7
mangojang994 <mangojang994@gmail.com>
2023-03-22 13:07:41 +0900

    Update : 레이아웃  css 수정

--- front/components/AppLayout/index.js
+++ front/components/AppLayout/index.js
@@ -6,7 +6,7 @@ import LoginForm from '../LoginForm';
 import UserProfile from '../UserProfile';
 import { LOAD_MYINFO_REQUEST, LOAD_USER_REQUEST } from '../../reducers/user';
 import { useRouter } from 'next/router';
-
+import { Container, Gnb, HomeIcon, Logo, LogoIcon, SideBarLeft, SideBarRight } from './style';
 
 
 const { Search } = Input;
@@ -53,19 +53,29 @@ const AppLayout = ({children}) => {
 
     return (
         <div>
-            <Menu mode="horizontal" items={items}/>
-            <Row gutter={10}>
-                <Col xs={24} md={6}>
-                    {mine?
-                    <UserProfile/>
-                    :
-                    <LoginForm/>
-                    }
+            {/* <Menu mode="horizontal" items={items}/> */}
+            <Row>
+                <SideBarLeft xs={3} sm={3} md={4} xl={6}>
+                    <div>
+                        <Logo><Link href="/" legacyBehavior><a><LogoIcon/></a></Link></Logo>
+                        <Gnb>
+                            <li><Link href="/"><div className='hover_area'><div className='gnb_icon'><HomeIcon /></div><div className='gnb_title'>홈</div></div></Link></li>
+                            {mine?<li><Link href="/profile"><div className='hover_area'><div className='gnb_icon'><ProfileIcon /></div><div className='gnb_title'>프로필</div></div></Link></li> : null }
+                            {mine? <li><UserProfile/></li> : null}
+                        </Gnb>
+                    </div>
+                </SideBarLeft>
+                <Col xs={21} sm={21} md={20} xl={18}>
+                    <Container>
+                        <div className='contents'>{children}</div>
+                        <SideBarRight className='sidebar_right'>
+                            <div>
+                                {mine? <Search placeholder="input search text" enterButton style={{ verticalAlign: 'middle' }} onSearch={onSearch} /> : <LoginForm/>}
+                                <p className='cmt'>본 사이트는 개인 포트폴리오용 사이트 입니다.</p>
+                            </div>
+                        </SideBarRight>
+                    </Container>
                 </Col>
-                <Col xs={24} md={12}>
-                    {children}
-                    </Col>
-                <Col xs={24} md={6}>세번째</Col>
             </Row>
         </div>
     );
---
+++ front/components/AppLayout/style.js
@@ -0,0 +1,147 @@
+import styled from 'styled-components';
+import { TwitterOutlined, HomeOutlined, UserOutlined } from '@ant-design/icons';
+import { Col } from 'antd';
+
+export const Logo = styled.h1`
+    a{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 52px;
+        height: 52px; 
+        border-radius: 50%;
+        /* color: #FCC419; */
+        color: #1D9BF0;
+        &:hover{
+            background-color: rgba(29, 155, 240, 0.1); 
+        }
+    }
+`;
+
+export const LogoIcon = styled(TwitterOutlined)`
+    svg{
+        width: 28px;
+        height: 100%;
+    }
+    &:hover{
+        background: initial;
+    }
+`;
+
+export const HomeIcon = styled(HomeOutlined)`
+    svg{
+        width: 28px;
+        height: 100%;
+    }
+    &:hover{
+        background: initial;
+    }
+`;
+
+export const ProfileIcon = styled(UserOutlined)`
+    svg{
+        width: 28px;
+        height: 100%;
+    }
+    &:hover{
+        background: initial;
+    }
+`;
+
+export const SideBarLeft = styled(Col)`
+    display: flex;
+    justify-content: flex-end;
+    border-right: 1px solid rgb(239, 243, 244);
+    >div{
+        position: fixed;
+        min-width: 275px;
+        padding: 8px;
+    }
+    @media (max-width: 1200px) {
+        >div{
+            min-width: initial;
+        }
+    }
+`;
+
+export const Container = styled.div`
+    display: flex;
+    .contents{
+        width: 600px;
+        @media (max-width: 1200px) {
+            width: 65%;
+        }
+    }
+    @media (max-width: 992px) {
+        flex-wrap: wrap;
+        .contents{
+            width: 100%;
+        }
+        .sidebar_right{
+            width: 100%;
+            background-color: pink
+        }
+    }
+`;
+
+export const SideBarRight = styled.div`
+    padding: 8px 16px;
+    border-left: 1px solid rgb(239, 243, 244);
+    >div{
+        position: fixed;
+        
+    }
+    .cmt{
+        font-size: 13px;
+        color: #536471;
+        line-height: 1.2;
+        word-break: keep-all;
+        margin: 16px 0;
+    }
+    @media (max-width: 992px) {
+        >div{
+            position: initial;
+        }
+    }
+`;
+
+export const Gnb = styled.ul`
+    li{
+        width: 100%;
+        height: 52px;
+        a{
+            display: block;
+            .hover_area{
+                display: inline-flex;
+                align-items: center;
+                border-radius: 9999px;
+                transition: background-color .2s ease;
+                padding: 12px ;
+                color: #0F1419;
+                .gnb_icon{
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+                .gnb_title{
+                    font-size: 20px;
+                    margin-left: 20px;
+                    margin-right: 16px;
+                    @media (max-width: 1200px) {
+                       display: none;
+                    }
+                }
+            }
+            &:hover{
+                .hover_area{
+                    background-color: #0F14191A;
+                }
+            }
+        }
+        &.on{
+            .gnb_title{
+                font-weight: 700;
+            }
+        }
+    }
+`;
