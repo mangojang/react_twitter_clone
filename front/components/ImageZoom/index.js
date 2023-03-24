8fdaaaa87b58a49b9600dfb28e79754755f646c4
mangojang994 <mangojang994@gmail.com>
2023-03-24 22:18:21 +0900

    Update : Image Zoom css 수정

--- front/components/ImageZoom/index.js
+++ front/components/ImageZoom/index.js
@@ -1,59 +1,45 @@
 import React, { useCallback, useState } from 'react';
 import Proptypes from 'prop-types';
-import { CloseOutlined } from '@ant-design/icons';
 import Slick from 'react-slick';
-import styled from 'styled-components';
-import  {Header, BtnClose } from './style';
+import  {Header, BtnClose, Zoom, Body } from './style';
 
-// const Header = styled.div`
-//     height: 44px; 
-//     background: white; 
-//     position: relative; 
-//     padding: 0; 
-//     text-align: center;
-// `;
-
-// const BtnClose = styled(CloseOutlined)`
-//     position: absolute;
-//     right: 0;
-//     top: 0;
-//     padding: 14px;
-//     line-height: 14px;
-//     cursor: pointer;
-// `;
 
 const ImageZoom = ({images, onClose}) => {
     const [currentSlide, setCurrentSlide] = useState(0);
 
     return (
-        <div style={{position: 'fixed', zIndex: 5000, top: 0, left: 0, right: 0, bottom: 0}}>
+        <Zoom>
             <Header>
-                <h1 style={{ margin: 0, fontSize: '17px', color: '#333', lineHeight: '44px'}}>상세이미지</h1>
-                <BtnClose onClick={onClose}/>
+                <h1 className='title'>상세이미지</h1>
+                <BtnClose onClick={onClose} size={'large'}/>
             </Header>
-            <div style={{height: 'calc(100% - 44px)', background: '#090909'}}>
-                <div>
+            <Body>
+                <div className='inner'>
                     <Slick
+                        adaptiveHeight={true}
                         initialSlide={0}
                         afterChange={(slide)=>{setCurrentSlide(slide)}}
                         infinite={false}
                         slidesToShow={1}
                         slidesToScroll={1}
+                        style={{outline: '1px solid white'}}
                     >
                         {images.map((v)=>{
                             return(
-                                <div style={{padding: 32, textAlign: 'center'}}>
-                                    <img src={`http://localhost:8000/${v.content}`} style={{margin:'0 auto', maxHeight:750}}/>
+                                <div className='image_box'>
+                                    <div>
+                                        <img src={`http://localhost:8000/${v.content}`}/>
+                                    </div>
                                 </div>
                             )
                         })}
                     </Slick>
-                    <div style={{textAlign: 'center'}}>
-                        <div style={{width:75, height:30, linheHeight:'30px', borderRadius:15, background:'#313131', display:'inline-block', textAlign:'center', color:'white', fontSize:'15px'}}>{currentSlide + 1} / {images.length}</div>
+                    <div className='pagination'>
+                        <span>{currentSlide + 1} / {images.length}</span>
                     </div>
                 </div>
-            </div>
-        </div>
+            </Body>
+        </Zoom>
     );
 };
 
--- front/components/ImageZoom/style.js
+++ front/components/ImageZoom/style.js
@@ -1,19 +1,69 @@
 import styled from 'styled-components';
 import { CloseOutlined } from '@ant-design/icons';
 
+export const Zoom = styled.div`
+    position: fixed;
+    z-index: 5000;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+`;
+
 export const Header = styled.div`
-    height: 44px; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 54px; 
     background: white; 
     position: relative; 
     padding: 0; 
     text-align: center;
+    .title{
+        font-size: 20px;
+        font-weight: 700;
+    }
 `;
 
 export const BtnClose = styled(CloseOutlined)`
     position: absolute;
     right: 0;
-    top: 0;
+    top: 5px;
     padding: 14px;
     line-height: 14px;
     cursor: pointer;
+`;
+
+export const Body = styled.div`
+    height: calc(100% - 44px);
+    background-color: #090909;
+    .inner{
+        height: 100%;
+        position: relative;
+        .image_box{
+            >div{
+                 display: flex;
+                align-items: center;
+                justify-content: center;
+                height: calc(100vh - 44px);
+            }
+            img{
+                border-radius: 0;
+            }
+        }
+        .pagination{
+            position: absolute;
+            bottom: 20px;
+            left: 50%;
+            transform: translateX(-50%);
+            text-align: center;
+            border-radius: 15px;
+            background-color: #313131;
+            padding: 4px 16px;
+            span{
+                color: white;
+            }
+
+        }
+    }
 `;
