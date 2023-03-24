8740c8e35c31afb5edbe1382e80470a6844bc99a
mangojang994 <mangojang994@gmail.com>
2023-03-24 18:22:03 +0900

    Update : UserProfile  더보기 버튼 css 수정

--- front/components/UserProfile/style.js
+++ front/components/UserProfile/style.js
@@ -6,7 +6,13 @@ export const UserProfleCard = styled.div`
         margin-bottom: 4px !important;
     }
     .btn_more{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 30px;
+        height: 30px;
         border-radius: 50%;
+        transition: background-color .2s ease-in;
         &:hover{
             background-color: #0F14191A;
         }
