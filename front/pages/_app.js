4fd81367059113b56193655ca57aae813ee511c2
mangojang994 <mangojang994@gmail.com>
2023-03-27 16:39:04 +0900

    Update : 폴리필 적용

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -25,6 +25,7 @@ const App = ({Component, ...rest }) => {
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"/>
                 <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
                 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+                <script src='https://polyfill.io/v3/polyfill.min.js?features=es5%2Ces6%2Ces7%2CNodeList.prototype.forEach&flags=gated'/>    
             </Head>
             <AppLayout>
                 <Reset />
