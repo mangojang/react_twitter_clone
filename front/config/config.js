52370d21df02b83d00f58281f628e4055e58728a
mangojang994 <mangojang994@gmail.com>
2023-04-05 16:43:30 +0900

    Fix: config.js

--- front/config/config.js
+++ front/config/config.js
@@ -1,4 +1,4 @@
-const backURL = process.env.NODE_ENV === 'production'? process.env.BACK_URL: 'http://localhost:8000';
-const frontURL = process.env.NODE_ENV === 'production'? process.env.FRONT_URL: 'http://localhost:3000';
+const backURL = process.env.NODE_ENV === 'production'? 'http://api.mangotwitter.site:8080': 'http://localhost:8000';
+const frontURL = process.env.NODE_ENV === 'production'? 'http://mangotwitter.site': 'http://localhost:3000';
 
 export { backURL, frontURL };
