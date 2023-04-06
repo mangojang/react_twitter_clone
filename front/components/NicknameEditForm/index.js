dbf325d57c36597c51bcd139c0b9d9b316a7a877
mangojang994 <mangojang994@gmail.com>
2023-04-06 22:43:59 +0900

    Update : nickname 수정 기능

--- front/components/NicknameEditForm/index.js
+++ front/components/NicknameEditForm/index.js
@@ -5,15 +5,19 @@ import { EDIT_NICKNAME_REQUEST } from '../../reducers/user';
 import { Container } from './style';
 
 const NicknameEditForm = () => {
-    const [ editName, setEditName] = useState('');
-    const dispatch = useDispatch();
     const { mine, isEditingNickname } = useSelector(state => state.user);
+    const [ editName, setEditName] = useState(mine? mine.nickname:'');
+    const dispatch = useDispatch();
 
     const onChangeNickname = useCallback((e)=>{
         setEditName(e.target.value);
     },[])
 
     const onEditNickname = useCallback(()=>{
+        if(editName.trim()==''){
+            alert('닉네임을 다시 확인해 주세요');
+            return false;
+        }
         dispatch({
             type: EDIT_NICKNAME_REQUEST,
             data: {nickname: editName},
@@ -24,7 +28,7 @@ const NicknameEditForm = () => {
             <Form onFinish={onEditNickname}>
                 <Form.Item>
                     <div className='row'>
-                        <Input className='input' addonBefore="닉네임" value={editName || (mine &&  mine.nickname)} onChange={onChangeNickname}/>
+                        <Input className='input' addonBefore="닉네임" value={editName} onChange={onChangeNickname}/>
                         <Button className='btn' type='primary' htmlType="submit" loading={isEditingNickname}>수정</Button>
                     </div>
                 </Form.Item>
